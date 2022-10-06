73a75ebd5e842093a61bee2ad34ab9fbe4cd2a18
cjhol2107 <cjhol2107@vntgcorp.com>
2022-10-06 15:04:08 +0900

    221006 최경호_사원조회 api 수정

--- src/apps/bzcm/query/EDUMGNT/100_EMP_list.sql
+++ src/apps/bzcm/query/EDUMGNT/100_EMP_list.sql
@@ -1,17 +1,15 @@
-
-select
-	emp_no,
-	user_id,
-	emp_name,
-	dept_code,
-	pang_sf_get_dept_name(dept_code) as dept_name,
-	email,
-	phon_number,
-	job,
-	responsi,
-	use_yn
-from
-	pang_emp_info
-where
-emp_name like '%%' || :p_emp_name || '%%'
-;
+SELECT  emp_no
+          ,user_id
+       ,emp_name
+       ,dept_code
+       ,pang_sf_get_dept_name(dept_code) as dept_name
+       ,email
+       ,phon_number
+       ,job
+       ,sf_get_code_name('CM31', job) as job_name
+       ,responsi
+       ,sf_get_code_name('CM27', responsi) as responsi_name
+       ,use_yn
+FROM pang_emp_info
+WHERE emp_name like '%%' || :p_emp_name || '%%';
+ORDER BY dept_code, emp_no;
